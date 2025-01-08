3fa8b8b705f82c082d9c2e6f13f1df605fe2b8d5
D-a-n-X <pokemonrue@gmail.com>
2025-01-08 14:31:19 +0700

    Can now remove twitter embeds and replace with FxTwitter link

--- src/main/java/wangbot/BotMain.java
+++ src/main/java/wangbot/BotMain.java
@@ -1,15 +1,20 @@
 package wangbot;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 
 public class BotMain {
     public static void main(String[] args) throws Exception {
 
-        String token = System.getenv("TOKEN");
+        String token = Dotenv.configure().directory("C:\\Users\\Dan X\\IdeaProjects\\wangbot").filename("token.env").load().get("TOKEN");
 
-        JDA api = JDABuilder.createDefault(token).build();
-
+        JDA api = JDABuilder.createDefault(token)
+                .addEventListeners(new Listener())
+                .enableIntents(GatewayIntent.MESSAGE_CONTENT)
+                .build();
 
     }
 }
--- src/main/java/wangbot/Listener.java
+++ src/main/java/wangbot/Listener.java
@@ -5,7 +5,14 @@ import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Listener extends ListenerAdapter {
+
+    //Hyperlink regex pattern
+    Pattern urlPattern = Pattern.compile("https?://\\S+", Pattern.CASE_INSENSITIVE);
+
     @Override
     public void onMessageReceived(MessageReceivedEvent event)
     {
@@ -18,12 +25,30 @@ public class Listener extends ListenerAdapter {
         // getContentRaw() is an atomic getter
         // getContentDisplay() is a lazy getter which modifies the content for e.g. console view (strip discord formatting)
 
+        MessageChannel channel = event.getChannel();
+
         //Basic ping-pong command
         if (content.equals("!ping"))
         {
-            MessageChannel channel = event.getChannel();
             channel.sendMessage("Pong!").queue();
             // Important to call .queue() on the RestAction returned by sendMessage(...)
+        }
+
+        //Regex pattern matching for hyperlinks
+        Matcher urlMatcher = urlPattern.matcher(content);
+        if (urlMatcher.find())
+        {
+            String url = urlMatcher.group(0);
+            //Check if the link is from twitter/X
+            if (url.contains("twitter.com") || url.contains("x.com")) {
+                //Replace with FxTwitter embed
+                String fix = url.replace("twitter.com", "fxtwitter.com")
+                                .replace("x.com", "fixupx.com");
+                channel.sendMessage(fix).queue(sentMessage -> {
+                    //Remove embed of previous message
+                    message.suppressEmbeds(true).queue();
+                });
+            }
         }
     }
 }
