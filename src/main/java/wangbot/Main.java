289f76126fa2b42efac877e32b633e195677ba5a
D-a-n-X <pokemonrue@gmail.com>
2025-01-08 16:26:51 +0700

    env stuff

--- src/main/java/wangbot/Main.java
+++ src/main/java/wangbot/Main.java
@@ -9,7 +9,7 @@ import net.dv8tion.jda.api.requests.GatewayIntent;
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        String token = System.getenv("TOKEN");
+        String token = Dotenv.load().get("TOKEN");
 
         JDA api = JDABuilder.createDefault(token)
                 .addEventListeners(new URLHandler())
