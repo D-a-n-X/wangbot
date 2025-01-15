91f583d8f9dc95c891c7ebaf051f60027748a377
D-a-n-X <pokemonrue@gmail.com>
2025-01-15 13:41:42 +0700

    Added detection of multiple links in one message

--- src/main/java/wangbot/URLHandler.java
+++ src/main/java/wangbot/URLHandler.java
@@ -4,6 +4,7 @@ import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -29,27 +30,38 @@ public class URLHandler extends ListenerAdapter {
 
         //Regex pattern matching for hyperlinks
         Matcher urlMatcher = urlPattern.matcher(content);
-        if (urlMatcher.find())
+
+        //StringBuilder to store the fixed message
+        StringBuilder fixedMessage = new StringBuilder();
+
+        while (urlMatcher.find())
         {
-            String url = urlMatcher.group(0);
-            //Check if the link is from twitter/X
-            if (url.contains("twitter.com") || url.contains("x.com")) {
-                //Replace with FxTwitter embed
-                String fix = url.replace("twitter.com", "fxtwitter.com")
-                                .replace("x.com", "fixupx.com");
-                channel.sendMessage(fix).queue(sentMessage -> {
-                    //Remove embed of previous message
-                    message.suppressEmbeds(true).queue();
-                });
-            }
-            //Check if the link is from pixiv
-            if (url.contains("pixiv.net")) {
-                //Replace with phixiv
-                String fix = url.replace("pixiv.net", "phixiv.net");
-                channel.sendMessage(fix).queue(sentMessage -> {
-                    message.suppressEmbeds(true).queue();
-                });
-            }
+            String fix = getString(urlMatcher);
+            urlMatcher.appendReplacement(fixedMessage, fix);
         }
+        urlMatcher.appendTail(fixedMessage);
+
+        channel.sendMessage(fixedMessage).queue(sentMessage -> {
+            message.suppressEmbeds(true).queue();
+        });
+    }
+
+    @NotNull
+    private static String getString(Matcher urlMatcher) {
+        String url = urlMatcher.group(0);
+        String fix = url;
+
+        //Check if the link is from twitter/X
+        if (url.contains("twitter.com") || url.contains("x.com")) {
+            //Replace with FxTwitter embed
+            fix = url.replace("twitter.com", "fxtwitter.com")
+                    .replace("x.com", "fixupx.com");
+        }
+        //Check if the link is from pixiv
+        if (url.contains("pixiv.net")) {
+            //Replace with phixiv
+            fix = url.replace("pixiv.net", "phixiv.net");
+        }
+        return fix;
     }
 }
